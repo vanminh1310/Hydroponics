029e4ad20e9ef2c61781898b6ad5af05718215a2
vanminh1310 <taminh39@gmail.com>
2021-01-17 17:48:15 +0700

    aaa

---
+++ guii/src/main.cpp
@@ -0,0 +1,217 @@
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+
+
+#define LVGL_TICK_PERIOD 60
+
+//Ticker tick; /* timer for interrupt handler */
+TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
+static lv_disp_buf_t disp_buf;
+static lv_color_t buf[LV_HOR_RES_MAX * 10];
+
+lv_obj_t * slider_label;
+int screenWidth = 320;
+int screenHeight = 240;
+  
+#if USE_LV_LOG != 0
+/* Serial debugging */
+void my_print(lv_log_level_t level, const char * file, uint32_t line, const char * dsc)
+{
+
+  Serial.printf("%s@%d->%s\r\n", file, line, dsc);
+  delay(100);
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint16_t c;
+
+  tft.startWrite(); /* Start new TFT transaction */
+  tft.setAddrWindow(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1)); /* set the working window */
+  for (int y = area->y1; y <= area->y2; y++) {
+    for (int x = area->x1; x <= area->x2; x++) {
+      c = color_p->full;
+      tft.writeColor(c, 1);
+      color_p++;
+    }
+  }
+  tft.endWrite(); /* terminate TFT transaction */
+  lv_disp_flush_ready(disp); /* tell lvgl that flushing is done */
+}
+
+bool my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
+{
+    uint16_t touchX, touchY;
+
+    bool touched = tft.getTouch(&touchX, &touchY, 600);
+
+    if(!touched)
+    {
+      return false;
+    }
+
+    if(touchX>screenWidth || touchY > screenHeight)
+    {
+      Serial.println("Y or y outside of expected parameters..");
+      Serial.print("y:");
+      Serial.print(touchX);
+      Serial.print(" x:");
+      Serial.print(touchY);
+    }
+    else
+    {
+
+      data->state = touched ? LV_INDEV_STATE_PR : LV_INDEV_STATE_REL; 
+  
+      /*Save the state and save the pressed coordinate*/
+      //if(data->state == LV_INDEV_STATE_PR) touchpad_get_xy(&last_x, &last_y);
+     
+      /*Set the coordinates (if released use the last pressed coordinates)*/
+      data->point.x = touchX;
+      data->point.y = touchY;
+  
+      //Serial.print("Data x");
+      //Serial.println(touchX);
+      
+     // Serial.print("Data y");
+     // Serial.println(touchY);
+
+    }
+
+    return false; /*Return `false` because we are not buffering and no more data to read*/
+}
+
+
+static void event_handler(lv_obj_t * obj, lv_event_t event)
+{
+    if(event == LV_EVENT_CLICKED) {
+        lv_ex_tabview_1();
+    }
+    else if(event == LV_EVENT_VALUE_CHANGED) {
+        Serial.println("Toggled\n");
+    }
+}
+static void event_handler1(lv_obj_t * obj, lv_event_t event)
+{
+    if(event == LV_EVENT_CLICKED) {
+         lv_ex_btn_1();
+    }
+    }
+
+static void lv_ex_tabview_1(void)
+{
+    /*Create a Tab view object*/
+    lv_obj_t *tabview;
+    tabview = lv_tabview_create(lv_scr_act(), NULL);
+
+    /*Add 3 tabs (the tabs are page (lv_page) and can be scrolled*/
+    lv_obj_t *tab1 = lv_tabview_add_tab(tabview, "Tab 1");
+    lv_obj_t *tab2 = lv_tabview_add_tab(tabview, "Tab 2");
+    lv_obj_t *tab3 = lv_tabview_add_tab(tabview, "Tab 3");
+
+
+    /*Add content to the tabs*/
+    lv_obj_t * label = lv_label_create(tab1, NULL);
+    lv_label_set_text(label, "This the first tab\n\n"
+                             "If the content\n"
+                        
+                             );
+
+    label = lv_label_create(tab2, NULL);
+    lv_label_set_text(label, "Second tab");
+    // tao nut bam su kien o day trong moi tap 
+    
+    lv_obj_t * btn13 = lv_btn_create(tab2, NULL);
+    lv_obj_align(btn13, NULL, LV_ALIGN_CENTER, 0, -40);
+
+    label = lv_label_create(btn13, NULL);
+    lv_label_set_text(label, "Exit");
+    lv_obj_set_event_cb(btn13, event_handler1);
+
+    label = lv_label_create(tab3, NULL);
+    lv_label_set_text(label, "Third tab");
+    
+
+  
+}
+
+
+void lv_ex_btn_1(void)
+{
+    lv_obj_t * label;
+
+    lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn1, event_handler);
+    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
+
+    label = lv_label_create(btn1, NULL);
+    lv_label_set_text(label, "Test");
+
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn2, event_handler);
+    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 40);
+    lv_btn_set_checkable(btn2, true);
+    lv_btn_toggle(btn2);
+    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
+
+    label = lv_label_create(btn2, NULL);
+    lv_label_set_text(label, "Toggled");
+}
+
+
+void setup() {
+
+  ledcSetup(10, 5000/*freq*/, 20 /*resolution - do phan giai*/);
+  ledcAttachPin(32, 10);
+  analogReadResolution(10);
+  ledcWrite(10,768);
+
+  Serial.begin(9600); /* prepare for possible serial debug */
+
+  lv_init();
+
+  #if USE_LV_LOG != 0
+    lv_log_register_print_cb(my_print); /* register print function for debugging */
+  #endif
+
+  tft.begin(); /* TFT init */
+  tft.setRotation(3);
+
+  uint16_t calData[5] = { 275, 3620, 264, 3532, 1 };
+  tft.setTouch(calData);
+
+  lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
+
+  /*Initialize the display - khoi tao man hinh */
+  lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.buffer = &disp_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  /*Initialize the input device driver- khoi tao trinh dieu khien thiet bi dau vao */
+  lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);             /*Descriptor of a input device driver*/
+  indev_drv.type = LV_INDEV_TYPE_POINTER;    /*Touch pad is a pointer-like device*/
+  indev_drv.read_cb = my_touchpad_read;      /*Set your driver function*/
+  lv_indev_drv_register(&indev_drv);         /*Finally register the driver*/
+
+  //Set the theme..dat chu de 
+  lv_theme_t * th = lv_theme_material_init(LV_THEME_DEFAULT_COLOR_PRIMARY, LV_THEME_DEFAULT_COLOR_SECONDARY, LV_THEME_DEFAULT_FLAG, LV_THEME_DEFAULT_FONT_SMALL , LV_THEME_DEFAULT_FONT_NORMAL, LV_THEME_DEFAULT_FONT_SUBTITLE, LV_THEME_DEFAULT_FONT_TITLE);     
+  lv_theme_set_act(th);
+
+  lv_obj_t * scr = lv_cont_create(NULL, NULL);
+  lv_disp_load_scr(scr);
+
+    lv_ex_btn_1();
+
+}
+void loop() {
+
+  lv_task_handler(); /* let the GUI do its work */
+  delay(5);
+}
