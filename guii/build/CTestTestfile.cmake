240ad6730217c11c7ee92af74efae454663c3a34
vanminh1310 <taminh39@gmail.com>
2021-02-01 09:47:39 +0700

    add cmake

---
+++ guii/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/IOT/JoyHydroponic/Hydroponics/guii
+# Build directory: E:/IOT/JoyHydroponic/Hydroponics/guii/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
